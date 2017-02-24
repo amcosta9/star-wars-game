5b2eb59329d5dfb7d5e0dc501914ccfb603291ed
Ariel <ariel.m.little@gmail.com>
2017-02-24 00:02:58 -0500

    game updates

--- assets/javascript/game2.js
+++ assets/javascript/game2.js
@@ -2,70 +2,124 @@
  * Created by Ariel on 2/22/2017.
  */
 $(document).ready(function() {
-    var characters = [];
+    var characters = [dvader, palpatine, chew, luke];
     var userChar = "";
     var enemies = [];
     var defender = "";
-    var pickDefender = true;
+    var pickDefender = false;
+
+    var dvader = {
+        name: "Darth Vader",
+        hp: 100,
+        attack: 100,
+        cAttack: 100
+    };
+    var palpatine = {
+        name: "Emperor Palpatine",
+        hp: 99,
+        attack: 100,
+        cAttack: 100
+    };
+    var chew = {
+        name: "Chewbacca",
+        hp: 98,
+        attack: 100,
+        cAttack: 100
+    };
+    var luke = {
+        name: "Luke Skywalker",
+        hp: 97,
+        attack: 100,
+        cAttack: 100
+    };
+
 
     //first character clicked is assigned as users character and moves to character div. other three move to enemies div and push to enemies array
     function pickChar () {
-        if (pickDefender) {
+        if (!pickDefender) {
             if ($("#darthVader-d").click(function () {
-                    $("#darthVader-d").appendTo("#yourCharacter-d");
-                    userChar = "dvader";
+                    $("#darthVader-d").appendTo("#yourCharacter-d").css({"background-color": "green"});
+                    $("#vaderHp-d").html("HP: " + dvader.hp);
+                    userChar = dvader;
                     console.log("your character: " + userChar);
-                    $("#palpatine-d").appendTo("#enemies-d");
-                    $("#chewbacca-d").appendTo("#enemies-d");
-                    $("#luke-d").appendTo("#enemies-d");
+                    $("#palpatine-d, #chewbacca-d, #luke-d").appendTo("#enemies-d").css({"background-color": "red"});
+                    $("#palpatineHp-d").html("HP: " + palpatine.hp);
+                    $("#lukeHp-d").html("HP: " + luke.hp);
+                    $("#chewbaccaHp-d").html("HP: " + chew.hp);
                     enemies.push("palp", "chew", "luke");
                     console.log("enemies are: " + enemies);
-                    pickDefender = false;
+                    pickDefender = true;
+                    console.log("pickDef " + pickDefender);
+                    pickDef();
                 }))
-                if ($("#palpatine-d").click(function () {
-                        $("#palpatine-d").appendTo("#yourCharacter-d");
-                        userChar = "palp";
-                        console.log("your character: " + userChar);
-                        $("#darthVader-d").appendTo("#enemies-d");
-                        $("#chewbacca-d").appendTo("#enemies-d");
-                        $("#luke-d").appendTo("#enemies-d");
-                        enemies.push("dvader", "chew", "luke");
-                        console.log("enemies are: " + enemies);
-                        pickDefender = false;
-                    }))
-                    if ($("#chewbacca-d").click(function () {
-                            $("#chewbacca-d").appendTo("#yourCharacter-d");
-                            userChar = "chew";
-                            console.log("your character: " + userChar);
-                            $("#palpatine-d").appendTo("#enemies-d");
-                            $("#darthVader-d").appendTo("#enemies-d");
-                            $("#luke-d").appendTo("#enemies-d");
-                            enemies.push("palp", "dvader", "luke");
-                            console.log("enemies are: " + enemies);
-                            pickDefender = false;
-                        }))
-                        if ($("#luke-d").click(function () {
-                                $("#luke-d").appendTo("#yourCharacter-d");
-                                userChar = "luke";
-                                console.log("your character: " + userChar);
-                                $("#palpatine-d").appendTo("#enemies-d");
-                                $("#chewbacca-d").appendTo("#enemies-d");
-                                $("#darthVader-d").appendTo("#enemies-d");
-                                enemies.push("palp", "chew", "dvader");
-                                console.log("enemies are: " + enemies);
-                                pickDefender = false;
-                            }));
-            pickDef();
-        }
+            if ($("#palpatine-d").click(function () {
+                   $("#palpatine-d").appendTo("#yourCharacter-d").css({"background-color": "green"});
+                   $("#palpatineHp-d").html("HP: " + palpatine.hp);
+                   userChar = palpatine;
+                   console.log("your character: " + userChar);
+                   $("#chewbacca-d, #darthVader-d, #luke-d").appendTo("#enemies-d").css({"background-color": "red"});
+                   $("#vaderHp-d").html("HP: " + dvader.hp);
+                   $("#lukeHp-d").html("HP: " + luke.hp);
+                   $("#chewbaccaHp-d").html("HP: " + chew.hp);
+                   enemies.push("dvader", "chew", "luke");
+                   console.log("enemies are: " + enemies);
+                   pickDefender = true;
+                   pickDef();
+            }))
+            if ($("#chewbacca-d").click(function () {
+                   $("#chewbacca-d").appendTo("#yourCharacter-d").css({"background-color": "green"});
+                   $("#chewbaccaHp-d").html("HP: " + chew.hp);
+                   userChar = chew;
+                   console.log("your character: " + userChar);
+                   $("#palpatine-d, #darthVader-d, #luke-d").appendTo("#enemies-d").css({"background-color": "red"});
+                   $("#palpatineHp-d").html("HP: " + palpatine.hp);
+                   $("#vaderHp-d").html("HP: " + dvader.hp);
+                   $("#lukeHp-d").html("HP: " + luke.hp);
+                   enemies.push("palp", "dvader", "luke");
+                   console.log("enemies are: " + enemies);
+                   pickDefender = true;
+                   pickDef();
+            }))
+            if ($("#luke-d").click(function () {
+                   $("#luke-d").appendTo("#yourCharacter-d").css({"background-color": "green"});
+                   $("#lukeHp-d").html("HP: " + luke.hp);
+                   userChar = luke;
+                   console.log("your character: " + userChar);
+                   $("#palpatine-d, #chewbacca-d, #darthVader-d").appendTo("#enemies-d").css({"background-color": "red"});
+                   $("#palpatineHp-d").html("HP: " + palpatine.hp);
+                   $("#vaderHp-d").html("HP: " + dvader.hp);
+                   $("#chewbaccaHp-d").html("HP: " + chew.hp);
+                   enemies.push("palp", "chew", "dvader");
+                   console.log("enemies are: " + enemies);
+                   pickDefender = true;
+                   pickDef();
+            }));
+        };
+
     };
 
     //characters clicked in enemies div selected to battle
     function pickDef() {
-        if (pickDefender = false) {
+        if (pickDefender) {
             if ($("#darthVader-d").click(function () {
                 $("#darthVader-d").appendTo("#defender-d");
                 defender = "dvader";
                 console.log("defender: " + defender);
+                }));
+            if ($("#palpatine-d").click(function () {
+                    $("#palpatine-d").appendTo("#defender-d");
+                    defender = "palpatine";
+                    console.log("defender: " + defender);
+                }));
+            if ($("#chewbacca-d").click(function () {
+                    $("#chewbacca-d").appendTo("#defender-d");
+                    defender = "chew";
+                    console.log("defender: " + defender);
+                }));
+            if ($("#luke-d").click(function () {
+                    $("#luke-d").appendTo("#defender-d");
+                    defender = "luke";
+                    console.log("defender: " + defender);
                 }));
         };
     }
